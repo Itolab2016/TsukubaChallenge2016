662e43849280df97f1653a09ae6296e7a116dd98
itolab <you@example.com>
2016-10-19 18:40:35 +0900

    camera修正

--- program/1019sasai/joystick_test.cpp
+++ program/1019sasai/joystick_test.cpp
@@ -2,9 +2,11 @@
 int main(){
 	joy_open();
 	while(1){
-		float a=get_joy_stickY ();
+//		float a=get_joy_stickY ();
+		float a=get_joy_button ();
 			if(a!=0){
-				if(a>0.9){
+				if(a==3){
+//				if(a>0.9){
 				cout<<"end"<<endl;
 				break;
 				}
