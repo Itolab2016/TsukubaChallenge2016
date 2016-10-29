493a8f82c933b43929ed14c30ad60f0265d8e85e
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2016-10-29 20:25:29 +0900

    replace some file

--- urg/tsukuba_urg.cpp
+++ urg/tsukuba_urg.cpp
@@ -3,15 +3,19 @@
 #include "math_utilities.h"
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 #include "tsukuba_urg.h"
+#define SAVEPOINT "urg_data.txt"
+#define MEASURMENTANGLE 180.0
 
 using namespace std;
 using namespace qrk;
 
 Urg_driver urg;
+ofstream urg_point(SAVEPOINT);
 
-void print_data(const Urg_driver& urg,
-    const vector<long>& data, long time_stamp)
+
+void print_data(const Urg_driver& urg, const vector<long>& data, long time_stamp)
 {
 #if 1
   // Shows only the front step
@@ -52,22 +56,43 @@ int urg_open()
     cout << "Urg_driver::open() error"<< endl;
     return 1;
   }
-  urg.set_scanning_parameter(urg.deg2step(-90), urg.deg2step(+90), 0);
+  urg.set_scanning_parameter(urg.deg2step(-MEASURMENTANGLE/2), urg.deg2step(+MEASURMENTANGLE/2), 0);
 }
 
 
 
 int get_urg_data()
 {
-  urg.start_measurement(Urg_driver::Distance, 1, 0);
   vector<long> data;
+	char str[255];
   long time_stamp = 0;
 
+	//計測司令
+  urg.start_measurement(Urg_driver::Distance, 1, 0);
+	
+	//計測データ取得
   if (!urg.get_distance(data, &time_stamp)) {
     cout << "Urg_driver::get_distance(): error" << endl;
     return 1;
   }
-  print_data(urg, data, time_stamp);
+	cout << data.size() << endl;
+  //print_data(urg, data, time_stamp);
 
+	double angle_step = MEASURMENTANGLE / (data.size() - 1);
+	
+	int k=0;	
+
+	for(int j=0;j<data.size();j++){
+		if(data[j] <= 20) continue;
+		double theta,pi,x,y;
+		pi=3.14159265359;
+		theta = angle_step*j + (-(MEASURMENTANGLE -180.0)/2.0);
+		x= data[j]*cos(theta*pi/180.0);
+		y= data[j]*sin(theta*pi/180.0);
+
+		sprintf(str,"%d %ld %lf %lf %lf,%ld",k,data[j],x,y,theta,time_stamp);
+		urg_point<<str<<endl;
+		k++;
+	}
   return 0;
 }
